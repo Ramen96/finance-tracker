ca94abc0e63e5b3e55dda59e8620ea1cfc291896
Ramen96 <eric.worsfold@gmail.com>
2025-12-01 21:33:12 -0500

    context for theme toggle works as expected now

--- context/context.tsx
+++ context/context.tsx
@@ -1,38 +1,46 @@
 "use client";
-
 import { createContext, useContext, useState, ReactNode, useEffect } from "react";
 
-export const ThemeContext = createContext<{ theme: 'light' | 'dark'; toggleTheme: () => void }>({
+type Theme = 'light' | 'dark';
+
+ interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+ }
+
+export const ThemeContext = createContext<ThemeContextType | undefined>({
   theme: 'light',
   toggleTheme: () => {},
 });
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light');
-  const stored = localStorage.getItem('theme');
-  const prefersDarkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  const [theme, setTheme] = useState<Theme>('light');
+  const [mounted, setMounted] = useState<boolean>(false);
 
-
-  function toggleTheme() {
-    if (theme === "light") {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-    document.documentElement.classList.toggle("dark");
-  };
-
-  function handleThemeChange(event) {
-    if (event.matches) {
-      setTheme('dark')
-    } else {
-      setTheme('light');
-    }
-  }
 
   useEffect(() => {
-    prefersDarkQuery.addEventListener('change', handleThemeChange);
+    setMounted(true);
+    const saved = localStorage.getItem('theme') as Theme;
+
+    if (saved) {
+      setTheme(saved);
+      document.body.setAttribute('data-theme', saved);
+    } else {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      const initial = prefersDark ? 'dark' : 'light';
+      setTheme(initial);
+      document.body.setAttribute('data-theme', initial);
+    }
   }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme)
+    localStorage.setItem('theme', newTheme);
+    document.body.setAttribute("data-theme", newTheme);
+  }
+
+  if (!mounted) return<>{ children }</>;
 
   return (
     <ThemeContext.Provider value ={{ theme, toggleTheme }}>
@@ -41,4 +49,8 @@ export const ThemeProvider = ({ children }: { children: ReactNode }) => {
   );
 }
 
-export const useTheme = () => useContext(ThemeContext);
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) throw new Error('useTheme must be used within ThemeProvider');
+  return context;
+}
