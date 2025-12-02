51e09db3fd1c208612bf54a778c104d6bed16454
Ramen96 <eric.worsfold@gmail.com>
2025-12-01 22:51:19 -0500

    created card for login component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,11 @@ import styles from "./Login.module.scss";
 
 export default function login() {
   return (
-    <main className={`flex-center-v h-full`}>
-      <h1>BEHOLD - the login page</h1>
+    <main className={`flex-center h-full w-full`}>
+      <section className={`${styles.card} flex-center-v`} id="card">
+        <input className="hidden" type="text" name="username" id="username" />
+        <label className={`${styles.textInput}`} htmlFor="username">Username</label>
+      </section>
     </main>
   );
 }
