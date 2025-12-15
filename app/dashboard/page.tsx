3e2536a2f17d5bbe6a86d2eda645b8e48f027d6a
Ramen96 <eric.worsfold@gmail.com>
2025-12-14 19:35:03 -0500

    centerd layout for ui cards and made the nav invisible just showing its content

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,8 +7,8 @@ export default function Dashboard () {
       <main className={styles.mainContainer}>
         <SideBar />
         <div id="content" className={styles.contentContainer}>
-          <nav id="nav" className={styles.nav}>
-            <h1>Goal and % to goal</h1>
+          <nav id="nav" className={`${styles.nav}`}>
+            <h1 className="">Goal and % to goal</h1>
           </nav>
           <section id="income" className={styles.card}>
             <h1>Income</h1>
@@ -20,12 +20,12 @@ export default function Dashboard () {
             <h1>Expenses</h1>
           </section>
           <section id="assets-and-liabilities" className={styles.card}>
-            <h1>Assets & Liabilites</h1>
+            <h1>Assets & Liabilities</h1>
             <span className={styles.cardItem}>
               <h1>Assets</h1>
             </span>
             <span className={styles.cardItem}>
-              <h1>Liabilites</h1>
+              <h1>Liabilities</h1>
               <li>
                 <ul>item1</ul>
                 <ul>item1</ul>
