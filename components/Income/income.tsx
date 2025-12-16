3a837f3d92cbbeba9de090f6484b7365224e5230
Ramen96 <eric.worsfold@gmail.com>
2025-12-15 21:52:59 -0500

    creating ui skeleton for income card

--- components/Income/income.tsx
+++ components/Income/income.tsx
@@ -1,5 +1,17 @@
+import styles from "./income.module.scss";
+
 export default function Income() {
   return (
-    <h1>Income</h1>
-  )
+    <div id="incomeContainer" className={`${styles.incomeContainer}`}>
+      <h1 className="w-full text-center">Income</h1>
+      <div className={styles.labelRow}>
+        <span className="text-center">Description</span>
+        <span className="text-center">Cash Flow</span>
+      </div>
+      <div className={styles.labelRowSecondary}>
+        <span className="text-center">Salary</span>
+        <span className="text-center">$$$$$$$</span>
+      </div>
+    </div>
+  );
 }
