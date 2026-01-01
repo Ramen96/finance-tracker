e73e749af6dc233c772c07b63d82419a62bbc9ec
Ramen96 <eric.worsfold@gmail.com>
2026-01-01 01:24:26 -0500

    added middle and bottom button groups to the sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { PanelLeftOpen, PanelLeftClose, ScanBarcode, CreditCard, LayoutDashboard } from "lucide-react";
+import { PanelLeftOpen, PanelLeftClose, ScanBarcode, CreditCard, LayoutDashboard, BanknoteArrowUp, BanknoteArrowDown, ChartCandlestick, ReceiptText, ShieldCheck, UserRoundPen, Settings } from "lucide-react";
 import styles from "./Sidebar.module.scss";
 
 type ExpandBtnPropType = {
@@ -9,7 +9,7 @@ type ExpandBtnPropType = {
   handleClick: () => void;
 }
 
-function ExpandBtn({ sideBarState, handleClick}: ExpandBtnPropType) {
+function ExpandBtn({ sideBarState, handleClick }: ExpandBtnPropType) {
   return (
     <span id="btn-container">
       <button onClick={handleClick} className={styles.btn} data-tooltip={sideBarState ? "Collapse Sidebar" : "Expand Sidebar"}>
@@ -25,16 +25,16 @@ export default function SideBar() {
 
   const toggleExpanded = () => setIsExpanded(!isExpanded);
 
-  const sideBarBtns = [
-    { 
-      id: 0, 
-      name: "Dashboard", 
+  const topButtons = [
+    {
+      id: 0,
+      name: "Dashboard",
       icon: <LayoutDashboard />,
       onClick: () => router.push("/dashboard")
     },
-    { 
-      id: 1, 
-      name: "Credit/Debit", 
+    {
+      id: 1,
+      name: "Credit/Debit",
       icon: <CreditCard />,
       onClick: () => router.push("/card")
     },
@@ -46,30 +46,113 @@ export default function SideBar() {
     },
   ];
 
+  const middleButtons = [
+    {
+      id: 3,
+      name: "Income",
+      icon: <BanknoteArrowUp />,
+      onClick: () => console.log("nothing for now")
+    },
+    {
+      id: 4,
+      name: "Expenses",
+      icon: <BanknoteArrowDown />,
+      onClick: () => console.log("nothing for now")
+    },
+    {
+      id: 5,
+      name: "Assets",
+      icon: <ChartCandlestick />,
+      onClick: () => console.log("nothing for now")
+    },
+    {
+      id: 6,
+      name: "Liabilities",
+      icon: <ReceiptText />,
+      onClick: () => console.log("nothing for now")
+    },
+    {
+      id: 7,
+      name: "Audit",
+      icon: <ShieldCheck />,
+      onClick: () => console.log("nothing for now")
+    },
+  ];
+
+  const bottomButtons = [
+    {
+      id: 8,
+      name: "Profile",
+      icon: <UserRoundPen />,
+      onClick: () => console.log("nothing for now")
+    },
+    {
+      id: 9,
+      name: "Settings",
+      icon: <Settings />,
+      onClick: () => console.log("nothing for now")
+    },
+  ]
+
   return (
     <section
-      className={`${styles.sideBarContainer} ${
-        isExpanded ? styles.wExpanded : styles.wContracted
-      }`}
+      className={`${styles.sideBarContainer} ${isExpanded ? styles.wExpanded : styles.wContracted
+        }`}
     >
       <ExpandBtn sideBarState={isExpanded} handleClick={toggleExpanded} />
       <section id="menuItems" className={styles.menu}>
-      {sideBarBtns.map((btn) => (
-        <span key={btn.id}>
-          <button 
-            data-tooltip={!isExpanded ? btn.name : undefined} 
-            className={styles.sideBarBtn}
-            onClick={btn.onClick}
+        {topButtons.map((btn) => (
+          <span key={btn.id}>
+            <button
+              data-tooltip={!isExpanded ? btn.name : undefined}
+              className={styles.sideBarBtn}
+              onClick={btn.onClick}
             >
-            {btn.icon}
-            {isExpanded && (
-              <p className={styles.sidebarBtnTxt}>
-                {btn.name}
-              </p>
-            )}
-          </button>
-        </span>
-      ))}
+              {btn.icon}
+              {isExpanded && (
+                <p className={styles.sidebarBtnTxt}>
+                  {btn.name}
+                </p>
+              )}
+            </button>
+          </span>
+        ))}
+      </section>
+      <section id="menuItems" className={styles.menu}>
+        {middleButtons.map((btn) => (
+          <span key={btn.id}>
+            <button
+              data-tooltip={!isExpanded ? btn.name : undefined}
+              className={styles.sideBarBtn}
+              onClick={btn.onClick}
+            >
+              {btn.icon}
+              {isExpanded && (
+                <p className={styles.sidebarBtnTxt}>
+                  {btn.name}
+                </p>
+              )}
+            </button>
+          </span>
+        ))}
+      </section>
+      <section id="menuItems" className={styles.menu}>
+        {bottomButtons.map((btn) => (
+          <span key={btn.id}>
+            <button
+              data-tooltip={!isExpanded ? btn.name : undefined}
+              className={styles.sideBarBtn}
+              onClick={btn.onClick}
+            >
+              {btn.icon}
+              {isExpanded && (
+                <p className={styles.sidebarBtnTxt}>
+                  {btn.name}
+                </p>
+              )}
+            </button>
+          </span>
+        ))}
       </section>
     </section>
   );
