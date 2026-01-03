75002b9817e78ed619358bf00a3525fc4fe7c0e1
Ramen96 <eric.worsfold@gmail.com>
2026-01-02 22:21:23 -0500

    created exports for Palette type usePalette and made it a client side component

--- context/usePalette.tsx
+++ context/usePalette.tsx
@@ -1,16 +1,14 @@
-import { useEffect, useState, ReactNode, createContext } from "react";
+"use client";
+import { useEffect, useState, ReactNode, createContext, useContext } from "react";
 
-type Palette = 'everforest' | 'nord' | 'catppuccin' | 'rose-pine' | 'zenburn';
+export type Palette = 'everforest' | 'nord' | 'catppuccin' | 'rose-pine' | 'zenburn';
 
 type PaletteContextType = {
   palette: Palette;
   setPalette: (palette: Palette) => void;
 }
 
-const PaletteContext = createContext<PaletteContextType>({
-  palette: 'everforest',
-  setPalette: () => { },
-});
+const PaletteContext = createContext<PaletteContextType | undefined>(undefined);
 
 export const PaletteProvider = ({ children }: { children: ReactNode }) => {
   const [palette, setPalette] = useState<Palette>(() => {
@@ -36,4 +34,10 @@ export const PaletteProvider = ({ children }: { children: ReactNode }) => {
       {children}
     </PaletteContext.Provider>
   );
+}
+
+export const usePalette = () => {
+  const context = useContext(PaletteContext);
+  if (!context) throw new Error('usePalette must be used within PaletteProvider');
+  return context;
 }
