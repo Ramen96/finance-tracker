01635e9c134152ec6061daac637a85d50aa7cab3
Ramen96 <eric.worsfold@gmail.com>
2026-02-07 13:36:02 -0500

    setup routing groups

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import AuthForm from "../../components/AuthForm/AuthForm";
+import AuthForm from "@/components/AuthForm/AuthForm";
 import { AuthProvider } from "@/context/useAuth";
 
 export default function Login() {
