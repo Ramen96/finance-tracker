16b34c3cbfe0058b253f6ea37fd084cae0f288c8
Ramen96 <eric.worsfold@gmail.com>
2026-02-16 16:06:28 -0500

    removed old components and refactoring with newer more simple overview ui

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,39 +1,68 @@
-import Income from "@/components/Income/income";
-import Audit from "@/components/Audit/audit";
-import Expenses from "@/components/Expenses/expenses";
-import Assets from "@/components/Assets/assets";
+"use client"
+import Loading from "@/components/Loading/loading";
+import { useEffect, useState } from "react";
 import styles from "./Dashboard.module.scss";
+import Audit from "@/components/Audit/audit";
 import Liabilities from "@/components/Liabilities/liabilities";
 
+
+// TODO:
+// 1. Simulate API call, fetching genaric total data for each section 
+// 2. Map over object to create simple overview 
+
+// PLACEHOLDER DATA
+
+const placeholderData = [
+  {
+    id: 1,
+    name: "Income",
+    amount: 1293109
+  },
+  {
+    id: 2,
+    name: "Expenses",
+    amount: 1231
+  },
+  {
+    id: 3,
+    name: "Assets",
+    amount: 400023
+  },
+  {
+    id: 4,
+    name: "Liabilities",
+    amount: 30203
+  }
+];
+
+
 export default function Dashboard() {
+
+  const [isLoading, setIsloading] = useState(true);
+  const [data, setData] = useState(false);
+
+  // Simulate API call
+  useEffect(() => {
+    setTimeout(() => {
+      setIsloading(false);
+    }, 500)
+  })
+
+  if (isLoading) {
+    return (
+      <Loading />
+    )
+  }
+
   return (
     <div id="content" className={styles.contentContainer}>
-      <div className={styles.cardsContainer}>
-        {/* Income Section */}
-        <section id="income" className={styles.card}>
-          <Income />
-        </section>
-
-        {/* Expenses Section */}
-        <section id="expenses" className={styles.card}>
-          <Expenses />
-        </section>
-
-        {/* Assets Section */}
-        <section id="assets-and-liabilities" className={styles.card}>
-          <Assets />
-        </section>
-
-        {/* Liabilities Section */}
-        <section id="liabilities" className={styles.card}>
-          <Liabilities />
-        </section>
-
-        {/* Audit Section */}
-        <section id="audit" className={styles.card}>
-          <Audit />
-        </section>
-      </div>
+      {placeholderData.map((element) =>
+        <div key={element.id}>
+          <h1>{element.name}</h1>
+          <h4>{element.amount}</h4>
+        </div>
+      )}
+      <Audit />
     </div>
   );
 }
