dcd9ff6236d801bd2edf7463a2c90c98b3b7e58f
Ramen96 <eric.worsfold@gmail.com>
2026-03-16 21:40:36 -0400

    mapping inputs based on columns

--- components/AddForm/addForm.tsx
+++ components/AddForm/addForm.tsx
@@ -1,21 +1,40 @@
 import styles from "./addForm.module.scss";
 
-export default function AddForm() {
+type AddFormPropTypes = {
+  columnConfig: any;
+}
+
+export default function AddForm({
+  columnConfig
+}: AddFormPropTypes) {
+
+  (() => {
+    columnConfig.map((e: any) => (
+      console.log(e.format())
+    ));
+  })();
+
+
   return (
     <div
       className={styles.tableRow}
       data-column-count={1}
     >
       <form>
-        <label htmlFor={"1"} className="sr-only">{ }</label>
-        <input
-          id="1"
-          name="input"
-          type="text"
-          className={`${styles.textInputPrimary} ${styles.withIcon}`}
-          placeholder="Item Name"
-        />
+        {columnConfig?.map((element: any) => (
+          <span key={element.key}>
+            <label htmlFor={"1"} className="sr-only">{ }</label>
+            <input
+              id={`${element.key}`}
+              name="input"
+              type="text"
+              className={`${styles.textInputPrimary} ${styles.withIcon}`}
+              placeholder={`${element.label}`}
+            />
+          </span>
+        ))}
+
       </form>
     </div>
-  )
+  );
 }
