cf2b534c1b2e72ed35c8eddb50f6d27ba16c707f
Ramen96 <eric.worsfold@gmail.com>
2026-03-19 22:11:06 -0400

    fixed alignment of inputs

--- components/AddForm/addForm.tsx
+++ components/AddForm/addForm.tsx
@@ -9,26 +9,30 @@ export default function AddForm({
 }: AddFormPropTypes) {
 
   return (
-    <div
+    <form
       className={styles.tableRow}
-      data-column-count={1}
+      data-column-count={columnConfig.filter((e: any) => e.inputType).length + 1}
     >
-      <form>
-        {columnConfig?.map((element: any) => (
-          element.inputType && (
-            <span key={element.key}>
-              <label htmlFor={`${element.key}`} className="sr-only">{ }</label>
-              <input
-                id={`${element.key}`}
-                name="input"
-                type={element.inputType === "number" ? "number" : "text"}
-                className={`${styles.textInputPrimary} ${styles.withIcon}`}
-                placeholder={`${element.label}`}
-              />
-            </span>
-          )
-        ))}
-      </form>
-    </div>
+      {columnConfig?.map((element: any) => (
+        element.inputType && (
+          <span
+            key={element.key}
+            data-table-grid={columnConfig.length}
+          >
+            <label htmlFor={`${element.key}`} className="sr-only">{ }</label>
+            <input
+              id={`${element.key}`}
+              name="input"
+              type={element.inputType === "number" ? "number" : "text"}
+              className={`${styles.textInputPrimary} ${styles.withIcon}`}
+              placeholder={`${element.label}`}
+            />
+          </span>
+        )
+      ))}
+      <span className={styles.actionsCol}>
+        <button type="submit">Save</button>
+      </span>
+    </form>
   );
 }
